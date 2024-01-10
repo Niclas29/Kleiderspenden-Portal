527fcae7ea81db60eb2f149aea37e1373e0cea84
Niclas <niclasdietz@gmail.com>
2024-01-10 11:21:01 +0100

    Header und Teil des Main Blocks auf der Landingpage implementiert.

---
+++ src/app/landing-main/landing-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-main.component.html',
+  styleUrl: './landing-main.component.scss'
+})
+export class LandingMainComponent{
+
+}
